24097b8b542d7937f97a9c975f24d8bd4cb944aa
Alan Chelko <achelko@gmail.com>
2022-09-28 18:16:09 +0000

    All tests successful

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -2,6 +2,7 @@
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const SECRET = 'TEST_SECRET';
 
 const userSchema = (sequelize, DataTypes) => {
   const model = sequelize.define('User', {
@@ -10,7 +11,8 @@ const userSchema = (sequelize, DataTypes) => {
     token: {
       type: DataTypes.VIRTUAL,
       get() {
-        return jwt.sign({ username: this.username });
+        return jwt.sign({ username: this.username }, SECRET,
+          {expiresIn: (1000 * 60 * 15)});
       },
     },
   });
